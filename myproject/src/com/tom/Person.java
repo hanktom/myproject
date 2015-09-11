ff2e0edf1f0d40fb35690ad3ed34e7380fb7f53f
Administrator <Administrator@WIN-AG7NR5LAI5T>
2015-09-11 18:57:09 +0800

    init

---
+++ myproject/src/com/tom/Person.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class Person {
+	
+	public void sayHello(){
+		System.out.println("Hello World");
+	}
+	
+	public static void main(String[] args) {
+		Person p = new Person();
+		p.sayHello();
+		
+		float w = (float)65.5;
+		float h = (float)1.7f;
+		
+		
+	}
+}
