23d1a3a7bba266f9a024e91307a4367bdba1bc65
Administrator <Administrator@WIN-AG7NR5LAI5T>
2015-09-11 21:45:37 +0800

    屬性、建構子

--- myproject/src/com/tom/Person.java
+++ myproject/src/com/tom/Person.java
@@ -1,18 +1,32 @@
 package com.tom;
 
 public class Person {
+	float weight;
+	float height;
+	public Person(float w, float h){
+		weight = w;
+		height = h;
+	}
+
+	public float bmi(){
+		float bmi = weight/(height*height);
+		return bmi;
+	}
 	
 	public void sayHello(){
 		System.out.println("Hello World");
 	}
 	
 	public static void main(String[] args) {
-		Person p = new Person();
-		p.sayHello();
+		Person p = new Person(65.5f, 1.7f);
+		System.out.println(p.bmi());
+//		p.sayHello();
+//		p.weight = 65.5f;
+//		p.height = 1.7f;
 		
-		float w = (float)65.5;
-		float h = (float)1.7f;
-		
-		
+		float w = 65.5f;
+		float h = 1.7f;
+		float bmi = w/(h*h);
+		System.out.println(bmi);
 	}
 }
