2d0554fd616095c0acc639963878d28c8cd52e4d
Hank Tom <hanktom@gmail.com>
2015-09-18 21:14:17 +0800

    Random類別與for迴圈測試

--- myproject/src/com/tom/Student.java
+++ myproject/src/com/tom/Student.java
@@ -4,7 +4,7 @@ public class Student {
 	int english;
 	int chinese;
 	int math;
-	int pass = 60;
+	static int pass = 60;
 	public Student(int eng, int chi, int m){
 		english = eng;
 		chinese = chi;
@@ -24,14 +24,18 @@ public class Student {
 		System.out.println(english+"\t"+chinese+"\t"+math+"\t"+average()+star);
 	}
 	
-	
 	public static void main(String[] args) {
 		Student stu1 = new Student(66, 77, 88);
 		Student stu2 = new Student(68, 33, 51);
 		Student stu3 = new Student(90, 94, 97);
-		stu1.report();
-		stu2.report();
-		stu3.report();
+		System.out.println(stu1.pass);
+		Student.pass = 55;
+//		stu1.pass
+		System.out.println(stu1.pass);
+		System.out.println(stu2.pass);
+//		stu1.report();
+//		stu2.report();
+//		stu3.report();
 		
 //		System.out.println(stu.average());
 	}
