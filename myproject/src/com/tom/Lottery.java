decce322ae04e53803465b1bafbcfc1413bbcf53
Hank Tom <hanktom@gmail.com>
2015-09-18 21:43:47 +0800

    檢查重複 validate方法(陣列、for、程設

--- myproject/src/com/tom/Lottery.java
+++ myproject/src/com/tom/Lottery.java
@@ -21,6 +21,22 @@ public class Lottery {
 		nums = numbers;
 	}
 	
+	public boolean validate(){
+		boolean valid = false;
+		int[] data = new int[40];
+		int count = 0;
+		for (int i=0; i<5; i++){
+			if (data[nums[i]]==0){
+				data[nums[i]] = 1;
+				count = count +1;
+			}
+		}
+		if (count==5)
+			valid = true;
+		
+		return valid;
+	}
+	
 	public void generate(){
 		Random r = new Random();
 		for (int i=0; i<5; i++){
@@ -42,6 +58,10 @@ public class Lottery {
 	
 	
 	public static void main(String[] args) {
+//		Lottery lot = new Lottery(1, 3, 7, 8, 9);
+		Lottery lot = new Lottery(1, 3, 7, 7, 9);
+		System.out.println(lot.validate());
+		
 		/*
 		Random r = new Random();
 		int[] n = new int[5];
