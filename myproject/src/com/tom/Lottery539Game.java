739a5d336dbc7705211fea2856f4f7a52592e354
Hank Tom <hanktom@gmail.com>
2015-09-22 22:00:23 +0800

    介紹類別改名與refactor

--- myproject/src/com/tom/Lottery539Game.java
+++ myproject/src/com/tom/Lottery539Game.java
@@ -2,23 +2,23 @@ package com.tom;
 
 import java.util.Random;
 
-public class Lottery{
-	int[] nums = new int[5];
+public class Lottery539Game{
+	int[] numbers = new int[5];
 	
-	public Lottery(){
+	public Lottery539Game(){
 		
 	}
 	
-	public Lottery(int n1, int n2, int n3, int n4, int n5){
-		nums[0] = n1;
-		nums[1] = n2;
-		nums[2] = n3;
-		nums[3] = n4;
-		nums[4] = n5;
+	public Lottery539Game(int n1, int n2, int n3, int n4, int n5){
+		numbers[0] = n1;
+		numbers[1] = n2;
+		numbers[2] = n3;
+		numbers[3] = n4;
+		numbers[4] = n5;
 	}
 	
-	public Lottery(int[] numbers){
-		nums = numbers;
+	public Lottery539Game(int[] numbers){
+		this.numbers = numbers;
 	}
 	
 	public boolean validate(){
@@ -26,8 +26,8 @@ public class Lottery{
 		int[] data = new int[40];
 		int count = 0;
 		for (int i=0; i<5; i++){
-			if (data[nums[i]]==0){
-				data[nums[i]] = 1;
+			if (data[numbers[i]]==0){
+				data[numbers[i]] = 1;
 				count = count +1;
 			}
 		}
@@ -40,7 +40,7 @@ public class Lottery{
 	public void generate(){
 		Random r = new Random();
 		for (int i=0; i<5; i++){
-			nums[i] = r.nextInt(39)+1;
+			numbers[i] = r.nextInt(39)+1;
 		}
 	}
 	
@@ -59,7 +59,7 @@ public class Lottery{
 	
 	public static void main(String[] args) {
 //		Lottery lot = new Lottery(1, 3, 7, 8, 9);
-		Lottery lot = new Lottery(1, 3, 7, 7, 9);
+		Lottery539Game lot = new Lottery539Game(1, 3, 7, 7, 9);
 		System.out.println(lot.validate());
 		
 		/*
