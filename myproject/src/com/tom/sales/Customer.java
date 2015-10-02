e77f3017f2aa9310702c973ee989279df8833d77
Hank Tom <hanktom@gmail.com>
2015-10-02 22:01:42 +0800

    封裝

--- myproject/src/com/tom/sales/Customer.java
+++ myproject/src/com/tom/sales/Customer.java
@@ -4,7 +4,14 @@ public abstract class Customer {
 	protected String name;
 	protected int id;
 	protected float discount = 0;
-	protected int discountLimit = 10000;
+	int discountLimit = 10000;
 	public abstract int calculcate(int amount);
 	
+	public float getDiscount(){
+		return discount;
+	}
+	public void setDiscount(float discount){
+		if (discount>0)
+			this.discount = discount;
+	}
 }
