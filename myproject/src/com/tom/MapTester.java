05d6ff31b0a62abe6d95bc28fcba1f14c03bdfc8
Hank Tom <hanktom@gmail.com>
2015-10-13 21:35:23 +0800

    集合-Map

---
+++ myproject/src/com/tom/MapTester.java
@@ -0,0 +1,29 @@
+package com.tom;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MapTester {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		map.put("2330", "台積電");
+		map.put("3008", "大立光");
+//		map.put("2330", "哈哈哈");
+		System.out.println(map);
+		System.out.println(map.size());
+		Set<String> keys = map.keySet();
+		for (String key: keys){
+			System.out.println(key);
+			System.out.println(map.get(key));
+		}
+		System.out.println(map.get("1234"));
+		if (map.containsKey("2330")){
+			
+		}
+		if (map.get("2330")!=null){
+			
+		}
+	}
+}
