674183f2ed5f2792cd155dc0e117a3b4f95cbb0f
Hank Tom <hanktom@gmail.com>
2015-10-16 21:23:44 +0800

    ParkingLot與Car類別(一)

--- myproject/src/com/tom/parking/Car.java
+++ myproject/src/com/tom/parking/Car.java
@@ -1,6 +1,9 @@
 package com.tom.parking;
 
+import java.text.SimpleDateFormat;
+
 public class Car {
+	static SimpleDateFormat sdf = new SimpleDateFormat("HHmm");
 	String id;
 	String enter;
 	String leave;
--- myproject/src/com/tom/parking/ParkingLot.java
+++ myproject/src/com/tom/parking/ParkingLot.java
@@ -1,7 +1,19 @@
 package com.tom.parking;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class ParkingLot {
 	int rate = 30;
+	Map<String, Date> map = new HashMap<>();
 	public void add(Car c){
 		
 	}
@@ -10,4 +22,24 @@ public class ParkingLot {
 		
 		return 0;
 	}
+	
+	public static void main(String[] args) {
+		try {
+			FileReader fr = new FileReader("parking-data2.txt");
+			BufferedReader in = new BufferedReader(fr);
+			String line = in.readLine();
+			while(line!=null){
+				System.out.println(line);
+				
+				new Scanner(System.in).nextLine();
+				line = in.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}	
 }
